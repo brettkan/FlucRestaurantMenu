ab116765880ef04024091338d086ddfea57f547e
brettkan <bkfighton@gmail.com>
2015-09-11 13:46:15 -0700

    add cartStore

---
+++ client/js/stores/cartStore.js
@@ -0,0 +1,36 @@
+var AppDispatcher = require('../dispatchers/appDispatcher');
+var appConstants = require('../constants/appConstants');
+var objectAssign = require('react/lib/Object.assign');
+var EventEmitter = require('events').EventEmitter;
+
+var CHANGE = 'CHANGE';
+
+var _state = {
+  cartItems: {}
+};
+
+var queueStore = objectAssign({}, EventEmitter.prototype, {
+  addChangeListener: function(cb){
+    this.on(CHANGE, cb);
+  },
+  removeChangeListener: function(cb){
+    this.removeListener(CHANGE, cb);
+  },
+  getState: function() {
+    return _state;
+  }
+});
+
+queueStore.dispatchToken = AppDispatcher.register(function(payload) {
+  if (payload.actionType === appConstants.ADD_TO_CART) {
+    addItemToCart(payload.data);
+    queueStore.emit(CHANGE);
+  } else if (payload.actionType === appConstants.REMOVE_FROM_CART ){
+    removeItemFromCart(payload.data);
+    queueStore.emit(CHANGE);
+  }
+
+  return true;
+});
+
+module.exports = queueStore;
