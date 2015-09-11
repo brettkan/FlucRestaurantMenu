068d6418ddc113aced8babbd2b5448a10e6b0948
brettkan <bkfighton@gmail.com>
2015-09-11 10:14:17 -0700

    add basic bootstrap navigation bar

---
+++ client/js/components/navBar.js
@@ -0,0 +1,26 @@
+var React = require('react');
+
+var NavBar = React.createClass({
+
+  render: function() {
+    return (
+      <nav className="navbar navbar-default navbar-fixed-top">
+        <div className="container-fluid">
+
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand page-scroll" href="#page-top">Fluc Yeah!</a>
+          </div>
+
+        </div>
+      </nav>
+    );
+  }
+});
+
+module.exports = NavBar;
